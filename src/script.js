dc73b004e0db5630d1c786bf00421d7142620df5
gostabohman@gmail.com <gostabohman@gmail.com>
2025-05-09 01:03:30 +0200

    Klar

--- src/script.js
+++ src/script.js
@@ -1,32 +1,39 @@
 // Definera array av namn
-let people;
+let people = ["Olivia", "Peter", "Walter", "Astrid"]
 
-
-export function initSearch(people) { // rör ej denna rad
+export function initSearch(people) {
+  // rör ej denna rad
 
   // Hämta HTML-element
-  
+  const searchInput = document.getElementById("searchInput")
+  const searchBtn = document.getElementById("searchBtn")
+  const resultDisplay = document.getElementById("resultDisplay")
+
   // Lägg till en klickhändelse på "Sök"-knappen
-  searchBtn.addEventListener("click",() => {
-  
+  searchBtn.addEventListener("click", () => {
     // 1. Hämta texten i searchInput.value
-    
+    const searchedText = searchInput.value
+
     // 2. Skapa en boolean för found
-    
+    let found = false
+
     // 3. Loopa igenom people-arrayen
-    
-    // 4. Jämför texten med varje namn i arrayen
-    
-    // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
-    
+    for (let person of people) {
+      // 4. Jämför texten med varje namn i arrayen
+      if (person === searchedText) {
+        // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
+        resultDisplay.innerHTML = "Namn hittades: " + person
+        found = true
+      }
+    }
     // 6. Om du efter loopen inte hittat något, skriv ut "Namn hittades inte." och gör found till false igen.
-    
-    });
+    if (!found) {
+      resultDisplay.innerHTML = "Namn hittades inte."
+    }
+  })
 }
-
-
 
 // rör ej dessa rader
 document.addEventListener("DOMContentLoaded", () => {
-  initSearch(people);
-});
+  initSearch(people)
+})
